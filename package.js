c151cb55e49ccc7ed83f9928b32fa1abc8b5076a
zodern <zodern@users.noreply.github.com>
2018-07-13 10:41:08 -0500

    Update pidusage to 1.1.0

--- package.js
+++ package.js
@@ -8,9 +8,8 @@ Package.describe({
 var npmModules = {
   "debug": "0.7.4",
   "kadira-core": "1.3.2",
-  "pidusage": "1.0.1",
   "evloop-monitor": "0.1.0",
-  "pidusage": "0.1.1",
+  "pidusage": "1.1.0",
   "lru-cache": "4.0.0",
   "json-stringify-safe": "5.0.1"
 };
