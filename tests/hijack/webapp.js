e1622c92493e729cdf71cee19eff397e3651ae5d
zodern <zodern@users.noreply.github.com>
2020-01-11 13:58:00 -0600

    Fix checking Meteor versions

--- tests/hijack/webapp.js
+++ tests/hijack/webapp.js
@@ -1,13 +1,13 @@
 import { checkHandlersInFiber } from "../../lib/hijack/wrap_webapp";
 
-const releaseParts = Meteor.release.split('.').map(num => {
+const releaseParts = Meteor.release.split('METEOR@')[1].split('.').map(num => {
   return parseInt(num, 10)
 })
 
 // Check if Meteor 1.6.1 or newer, which are the
 // versions that wrap connect handlers in a fiber
 const httpMonitoringEnabled = releaseParts[0] > 0 &&
-  releaseParts[1] === 6 ? releaseParts[2] > 0 : releaseParts[1] > 5 
+  (releaseParts[1] === 6 ? releaseParts[2] > 0 : releaseParts[1] > 6)
 
 Tinytest.add(
   'Webapp - checkHandlersInFiber',
