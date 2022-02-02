ba3f849cc59f55868b648944135e3ce67d79b2af
harryadel <harryadelb@gmail.com>
2022-02-02 21:31:20 +0200

    Set values again to a 100

--- tests/mongo_driver_events.js
+++ tests/mongo_driver_events.js
@@ -2,11 +2,6 @@ import { getMongoDriverStats, resetMongoDriverStats } from '../lib/hijack/mongo-
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
-// It can be as simple as calling getMongoDriverStats and resetMongoDriverStats and making sure the return values are reasonable.
-// This won't catch everything, but will at least make sure the file won't
-// cause an app to crash on startup. We will probably have to manually review Mongo's changelog
-// and test this feature to make sure it works with the new mongo driver.
-
 Tinytest.addAsync(
   'Mongo Driver Events - getMongoDriverStats',
   function (test) {
@@ -32,7 +27,7 @@ Tinytest.addAsync(
     resetMongoDriverStats();
     const stats = getMongoDriverStats();
     test.equal(stats, {
-      poolSize: 0,
+      poolSize: 100,
       primaryCheckouts: 0,
       otherCheckouts: 0,
       checkoutTime: 0,
@@ -45,7 +40,7 @@ Tinytest.addAsync(
     await delay(5000);
     const delayedStats = getMongoDriverStats();
     test.equal(delayedStats, {
-      poolSize: 0,
+      poolSize: 100,
       primaryCheckouts: 0,
       otherCheckouts: 0,
       checkoutTime: 0,
@@ -58,7 +53,7 @@ Tinytest.addAsync(
     resetMongoDriverStats();
     const postResetStats = getMongoDriverStats();
     test.equal(postResetStats, {
-      poolSize: 0,
+      poolSize: 100,
       primaryCheckouts: 0,
       otherCheckouts: 0,
       checkoutTime: 0,
