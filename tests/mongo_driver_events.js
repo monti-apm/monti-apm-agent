35a2ef4ae13554e4ab7e55eacbf33bb7d0a2d7ea
harryadel <harryadelb@gmail.com>
2022-02-03 01:16:57 +0200

    test

--- tests/mongo_driver_events.js
+++ tests/mongo_driver_events.js
@@ -8,13 +8,51 @@ const mongoMonitoringEnabled = releaseParts[0] > 1 ||
   (releaseParts[0] > 1 && releaseParts[1] > 1)
 
 if (mongoMonitoringEnabled) {
-
   Tinytest.addAsync(
     'Mongo Driver Events - getMongoDriverStats',
     function (test) {
       const stats = getMongoDriverStats();
+      test.equal(stats,
+        {
+          poolSize: 10,
+          primaryCheckouts: 0,
+          otherCheckouts: 0,
+          checkoutTime: 0,
+          maxCheckoutTime: 0,
+          pending: NaN,
+          checkedOut: NaN,
+          created: 1,
+          measurementCount: 0
+        }
+
+      );
+    }
+  );
+
+
+  Tinytest.addAsync(
+    'Mongo Driver Events - resetMongoDriverStats',
+    async function (test, done) {
+      resetMongoDriverStats();
+      const stats = getMongoDriverStats();
+      // immediately checking for poolsize after triggering getMongoDriverStats should be zerob 
       test.equal(stats, {
-        poolSize: 100,
+        poolSize: 0,
+        primaryCheckouts: 0,
+        otherCheckouts: 0,
+        checkoutTime: 0,
+        maxCheckoutTime: 0,
+        pending: NaN,
+        checkedOut: NaN,
+        created: 0,
+        measurementCount: 0
+      });
+      test.equal(typeof stats.poolSize, 'number')
+      await delay(5000);
+      const delayedStats = getMongoDriverStats();
+      // pool size value isn't consistent across all versions so we can't expect a certain number
+      test.equal(delayedStats, {
+        poolSize: stats.poolSize,
         primaryCheckouts: 0,
         otherCheckouts: 0,
         checkoutTime: 0,
@@ -22,61 +60,25 @@ if (mongoMonitoringEnabled) {
         pending: 0,
         checkedOut: 0,
         created: 0,
-        measurementCount: 1
+        measurementCount: 5
       });
+      test.equal(typeof delayedStats.poolSize, 'number')
+      resetMongoDriverStats();
+      const postResetStats = getMongoDriverStats();
+      test.equal(postResetStats, {
+        poolSize: stats.poolSize,
+        primaryCheckouts: 0,
+        otherCheckouts: 0,
+        checkoutTime: 0,
+        maxCheckoutTime: 0,
+        pending: NaN,
+        checkedOut: NaN,
+        created: 0,
+        measurementCount: 0
+      });
+      test.equal(typeof postResetStats.poolSize, 'number')
+      done();
     }
   );
-
-
-  // Tinytest.addAsync(
-  //   'Mongo Driver Events - resetMongoDriverStats',
-  //   async function (test, done) {
-  //     resetMongoDriverStats();
-  //     const stats = getMongoDriverStats();
-  //     // immediately checking for poolsize after triggering getMongoDriverStats should be zerob 
-  //     test.equal(stats, {
-  //       poolSize: 0,
-  //       primaryCheckouts: 0,
-  //       otherCheckouts: 0,
-  //       checkoutTime: 0,
-  //       maxCheckoutTime: 0,
-  //       pending: NaN,
-  //       checkedOut: NaN,
-  //       created: 0,
-  //       measurementCount: 0
-  //     });
-  //     test.equal(typeof stats.poolSize, 'number')
-  //     await delay(5000);
-  //     const delayedStats = getMongoDriverStats();
-  //     // pool size value isn't consistent across all versions so we can't expect a certain number
-  //     test.equal(delayedStats, {
-  //       poolSize: stats.poolSize,
-  //       primaryCheckouts: 0,
-  //       otherCheckouts: 0,
-  //       checkoutTime: 0,
-  //       maxCheckoutTime: 0,
-  //       pending: 0,
-  //       checkedOut: 0,
-  //       created: 0,
-  //       measurementCount: 5
-  //     });
-  //     test.equal(typeof delayedStats.poolSize, 'number')
-  //     resetMongoDriverStats();
-  //     const postResetStats = getMongoDriverStats();
-  //     test.equal(postResetStats, {
-  //       poolSize: stats.poolSize,
-  //       primaryCheckouts: 0,
-  //       otherCheckouts: 0,
-  //       checkoutTime: 0,
-  //       maxCheckoutTime: 0,
-  //       pending: NaN,
-  //       checkedOut: NaN,
-  //       created: 0,
-  //       measurementCount: 0
-  //     });
-  //     test.equal(typeof postResetStats.poolSize, 'number')
-  //     done();
-  //   }
-  // );
 
 }
