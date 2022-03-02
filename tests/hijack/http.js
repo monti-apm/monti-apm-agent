ad707ef7f55ab7b8ffb063fb67cf4bc1ea8f95cd
afrokick <devbyru@gmail.com>
2022-03-02 23:53:18 +0300

    modernize http

--- tests/hijack/http.js
+++ tests/hijack/http.js
@@ -1,15 +1,15 @@
-Tinytest.add(
-  'HTTP - call a server',
-  function (test) {
-    EnableTrackingMethods();
-    var methodId = RegisterMethod(function () {
-      var result = HTTP.get('http://localhost:3301');
+import { HTTP } from 'meteor/http';
+const Future = Npm.require('fibers/future');
+
+Tinytest.add('HTTP - call a server', function (test) {
+    const methodId = RegisterMethod(function () {
+      const result = HTTP.get('http://localhost:3301');
       return result.statusCode;
     });
-    var client = GetMeteorClient();
-    var result = client.call(methodId);
-    var events = GetLastMethodEvents([0, 2]);
-    var expected = [
+    const client = GetMeteorClient();
+    const result = client.call(methodId);
+    const events = GetLastMethodEvents([0, 2]);
+    const expected = [
       ['start', , { userId: null, params: '[]' }],
       ['wait', , { waitOn: [] }],
       ['http', , { url: 'http://localhost:3301', method: 'GET', statusCode: 200 }],
@@ -21,15 +21,10 @@ Tinytest.add(
   }
 );
 
-
-Tinytest.add(
-  'HTTP - async callback',
-  function (test) {
-    EnableTrackingMethods();
-    var methodId = RegisterMethod(function () {
-      var Future = Npm.require('fibers/future');
-      var f = new Future();
-      var result;
+Tinytest.add('HTTP - async callback', function (test) {
+    const methodId = RegisterMethod(function () {
+      const f = new Future();
+      let result;
       HTTP.get('http://localhost:3301', function (err, res) {
         result = res;
         f.return();
@@ -37,10 +32,10 @@ Tinytest.add(
       f.wait();
       return result.statusCode;
     });
-    var client = GetMeteorClient();
-    var result = client.call(methodId);
-    var events = GetLastMethodEvents([0, 2]);
-    var expected = [
+    const client = GetMeteorClient();
+    const result = client.call(methodId);
+    const events = GetLastMethodEvents([0, 2]);
+    const expected = [
       ['start', , { userId: null, params: '[]' }],
       ['wait', , { waitOn: [] }],
       ['http', , { url: 'http://localhost:3301', method: 'GET', async: true }],
