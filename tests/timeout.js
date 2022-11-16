a114f815a0639f3cdd14eaee6487a2c3b4eac147
Leonardo Venturini <leovenbag@gmail.com>
2022-11-16 09:18:54 -0400

    adjust message

--- tests/timeout.js
+++ tests/timeout.js
@@ -28,7 +28,7 @@ Tinytest.add(
 
     assert(lastId < TimeoutManager.id, 'The timeout id must be incremented');
     assert(error && error.constructor.name === 'Error');
-    assert(error && error.message === `Method still running after 250ms: ${methodId}`);
+    assert(error && error.message === `Method "${methodId}" still running after 250ms`);
 
     test.equal(result, 'pong');
 
