b2f86a37770cdfa8a6964ead14cfea199bb9f3b5
Leonardo Venturini <leovenbag@gmail.com>
2022-11-25 16:02:59 -0400

    include and refactor event loop monitor

--- package.js
+++ package.js
@@ -10,7 +10,6 @@ Package.describe({
 let npmModules = {
   debug: '0.8.1',
   'monti-apm-core': '1.7.5',
-  'evloop-monitor': '0.1.0',
   'lru-cache': '5.1.1',
   'json-stringify-safe': '5.0.1',
   'monti-apm-sketches-js': '0.0.3',
@@ -76,6 +75,7 @@ Package.onTest(function (api) {
     'tests/environment_variables.js',
     'tests/docsize_cache.js',
     'tests/timeout.js',
+    'tests/event_loop_monitor.js',
   ], 'server');
 
   if (canRunTestsWithFetch()) {
