ed5d5062d4fac40a00e4efef158fb45ce904a7ca
Leonardo Venturini <leovenbag@gmail.com>
2023-04-10 10:06:10 -0400

    refactor http tests

--- tests/hijack/http.js
+++ tests/hijack/http.js
@@ -1,40 +1,47 @@
 import { HTTP } from 'meteor/http';
-import { CleanTestData, GetLastMethodEvents, GetMeteorClient, RegisterMethod } from '../_helpers/helpers';
+import {
+  callAsync,
+  cleanTestData,
+  getLastMethodEvents,
+  GetLastMethodEvents,
+  RegisterMethod
+} from '../_helpers/helpers';
 
-Tinytest.add('HTTP - meteor/http - call a server', function (test) {
+Tinytest.addAsync('HTTP - meteor/http - call a server', async function (test, done) {
   const methodId = RegisterMethod(function () {
     const result = HTTP.get('http://localhost:3301');
     return result.statusCode;
   });
-  const client = GetMeteorClient();
-  const result = client.call(methodId);
-  const events = GetLastMethodEvents([0, 2]);
+
+  const result = await callAsync(methodId);
+
+  const events = getLastMethodEvents([0, 2]);
+
   const expected = [
     ['start', undefined, { userId: null, params: '[]' }],
     ['wait', undefined, { waitOn: [] }],
     ['http', undefined, { url: 'http://localhost:3301', method: 'GET', statusCode: 200, library: 'meteor/http' }],
     ['complete']
   ];
+
   test.equal(events, expected);
   test.equal(result, 200);
-  CleanTestData();
+
+  await cleanTestData();
+
+  done();
 }
 );
 
-Tinytest.add('HTTP - meteor/http - async callback', function (test) {
+Tinytest.addAsync('HTTP - meteor/http - async callback', async function (test, done) {
   const methodId = RegisterMethod(function () {
-    const f = new Future();
-    let result;
-    HTTP.get('http://localhost:3301', function (err, res) {
-      result = res;
-      f.return();
-    });
-    f.wait();
+    const result = HTTP.get('http://localhost:3301');
     return result.statusCode;
   });
-  const client = GetMeteorClient();
-  const result = client.call(methodId);
+
+  const result = await callAsync(methodId);
   const events = GetLastMethodEvents([0, 2]);
+
   const expected = [
     ['start', undefined, { userId: null, params: '[]' }],
     ['wait', undefined, { waitOn: [] }],
@@ -42,8 +49,11 @@ Tinytest.add('HTTP - meteor/http - async callback', function (test) {
     ['async', undefined, {}],
     ['complete']
   ];
+
   test.equal(events, expected);
   test.equal(result, 200);
-  CleanTestData();
+
+  await cleanTestData();
+  done();
 }
 );
