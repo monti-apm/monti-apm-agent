6f74c718e066264de23c762bcb3ee3cb31841999
Leonardo Venturini <leovenbag@gmail.com>
2023-04-11 10:07:47 -0400

    revert user test

--- tests/hijack/user.js
+++ tests/hijack/user.js
@@ -1,16 +1,16 @@
 import { TestData } from '../_helpers/globals';
 import { callAsync, cleanTestData, getLastMethodEvents, registerMethod } from '../_helpers/helpers';
 
-Tinytest.onlyAsync(
+Tinytest.addAsync(
   'User - not logged in',
   async function (test, done) {
     let methodId = registerMethod(async function () {
-      console.log(await TestData.insertAsync({aa: 10}));
+      await TestData.insertAsync({aa: 10});
 
       return 'foo';
     });
 
-    console.log(await callAsync(methodId));
+    await callAsync(methodId);
 
     let events = getLastMethodEvents([0, 2]);
 
