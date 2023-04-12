7e6fa294fc1a5ef09ac441674268e23c2df1ed9f
Leonardo Venturini <leovenbag@gmail.com>
2023-04-12 09:36:30 -0400

    add object diffing capabilities

--- tests/_helpers/pretty-log.js
+++ tests/_helpers/pretty-log.js
@@ -1,6 +1,41 @@
 import chalk from 'chalk';
 import highlight from 'cli-highlight';
 import { stringify } from 'yaml';
+import Diff from 'diff';
+
+export function diffStrings (a, b) {
+  const diff = Diff.diffLines(a, b);
+
+  let result = '';
+
+  diff.forEach((part) => {
+    const color = part.added ? 'green' : part.removed ? 'red' : 'grey';
+    result += chalk[color](part.value);
+  });
+
+  return result;
+}
+
+export function sortedStringify (obj) {
+  return stringify(obj, (_, value) => {
+    if (value && typeof value === 'object' && !Array.isArray(value)) {
+      return Object.keys(value)
+        .sort()
+        .reduce((sorted, key) => {
+          sorted[key] = value[key];
+          return sorted;
+        }, {});
+    }
+    return value;
+  });
+}
+
+export function diffObjects (a, b) {
+  const yamlA = sortedStringify(a);
+  const yamlB = sortedStringify(b);
+
+  console.log(diffStrings(yamlA, yamlB));
+}
 
 export function prettyLog (...args) {
   const label = args.length > 1 ? args[0] : '';
