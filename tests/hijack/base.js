37ab823f46168f067a2673e6c0e00bc761803331
Leonardo Venturini <leovenbag@gmail.com>
2023-05-16 10:22:54 -0400

    remove hooks capture

--- tests/hijack/base.js
+++ tests/hijack/base.js
@@ -1,21 +1,15 @@
 import { TestData } from '../_helpers/globals';
 import { addAsyncTest, callAsync, getLastMethodEvents, registerMethod } from '../_helpers/helpers';
-import { prettyLog } from '../_helpers/pretty-log';
 
 
 addAsyncTest(
   'Base - method params',
   async function (test) {
-    let info;
-
     let methodId = registerMethod(async function () {
       await TestData.insertAsync({aa: 10});
-      info = Kadira._getInfo();
     });
 
     await callAsync(methodId, 10, 'abc');
-
-    prettyLog(info);
 
     let events = getLastMethodEvents([0, 2]);
 
