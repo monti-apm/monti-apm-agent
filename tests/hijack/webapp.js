e11fa88faf9f36ebd86d5497a3e978c68117ba26
Leonardo Venturini <leovenbag@gmail.com>
2023-09-08 10:03:14 -0400

    properly migrate webapp logic to new express impl

--- tests/hijack/webapp.js
+++ tests/hijack/webapp.js
@@ -1,6 +1,6 @@
 import { WebApp } from 'meteor/webapp';
 import { wrapWebApp } from '../../lib/hijack/wrap_webapp';
-import { releaseParts } from '../_helpers/helpers';
+import { addAsyncTest, releaseParts } from '../_helpers/helpers';
 
 // Check if Meteor 1.7 or newer, which are the
 // versions that wrap connect handlers in a fiber and are easy
@@ -11,20 +11,20 @@ const httpMonitoringEnabled = releaseParts[0] > 1 ||
 if (httpMonitoringEnabled) {
   wrapWebApp();
 
-  Tinytest.add(
+  addAsyncTest(
     'Webapp - return connect app from .use',
-    function (test) {
-      const result = WebApp.connectHandlers.use((req, res, next) => {
+    async function (test) {
+      const result = WebApp.expressHandlers.use((req, res, next) => {
         next();
       });
 
-      test.equal(result, WebApp.connectHandlers);
+      test.equal(result, WebApp.expressHandlers);
     }
   );
 
-  Tinytest.addAsync(
+  addAsyncTest(
     'Webapp - filter headers',
-    function (test, done) {
+    async function (test) {
       Kadira.tracer.redactField('x--test--authorization');
 
       let req = {
@@ -37,17 +37,21 @@ if (httpMonitoringEnabled) {
         }
       };
 
-      WebApp.rawConnectHandlers.stack[0].handle(
-        req,
-        {on () {}},
-        function () {
-          const expected = JSON.stringify({
-            'content-type': 'application/json',
-            'content-length': '1000',
-            'x--test--authorization': 'Monti: redacted'
+      const firstMiddleware = WebApp.rawExpressHandlers.parent._router.stack[0].handle;
+
+      await new Promise((resolve) => {
+        firstMiddleware(
+          req,
+          { on () {} },
+          function () {
+            const expected = JSON.stringify({
+              'content-type': 'application/json',
+              'content-length': '1000',
+              'x--test--authorization': 'Monti: redacted'
+            });
+            test.equal(req.__kadiraInfo.trace.events[0].data.headers, expected);
+            resolve();
           });
-          test.equal(req.__kadiraInfo.trace.events[0].data.headers, expected);
-          done();
-        });
+      });
     });
 }
