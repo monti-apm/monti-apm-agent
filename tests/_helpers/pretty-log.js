e39731a263b78c268c9fd273398c0b637ca2d86d
Leonardo Venturini <leovenbag@gmail.com>
2023-10-02 14:23:49 -0400

    remove null coalesce

--- tests/_helpers/pretty-log.js
+++ tests/_helpers/pretty-log.js
@@ -41,13 +41,13 @@ export function prettyLog (...args) {
   const label = args.length > 1 ? args[0] : '';
   const obj = args.length > 1 ? args[1] : args[0];
 
-  const yaml = stringify(obj);
+  const yaml = stringify(obj) || '';
 
   if (label) {
     console.log(`${chalk.yellowBright(`${label}`)}`);
   }
 
   console.log(
-    highlight(yaml?.trim() ?? '', { language: 'yaml', ignoreIllegals: true }),
+    highlight(yaml.trim(), { language: 'yaml', ignoreIllegals: true }),
   );
 }
