d57690e095807063b7028c29a84bf40dcda8599e
Leonardo Venturini <leovenbag@gmail.com>
2023-10-10 12:21:59 -0400

    add test for removed docs

--- tests/hijack/redis_oplog.js
+++ tests/hijack/redis_oplog.js
@@ -1,6 +1,5 @@
 import { TestData } from '../_helpers/globals';
 import { GetMeteorClient, RegisterPublication, SubscribeAndWait } from '../_helpers/helpers';
-import { prettyLog } from '../_helpers/pretty-log';
 
 /**
  * We only track the observers coming from subscriptions (which have `ownerInfo`)
@@ -25,7 +24,41 @@ Tinytest.add('Database - Redis Oplog - Added', function (test) {
   test.equal(metrics.oplogInsertedDocuments, 3);
   test.equal(metrics.liveAddedDocuments, 3);
 
-  prettyLog(metrics);
+  sub.stop();
+  TestData.remove({});
+});
+
+Tinytest.add('Database - Redis Oplog - Removed', function (test) {
+  const pub = RegisterPublication(() => TestData.find({}));
+
+  TestData.remove({});
+
+  const client = GetMeteorClient();
+  const sub = SubscribeAndWait(client, pub);
+
+  TestData.insert({ name: 'test1' });
+  TestData.insert({ name: 'test2' });
+  TestData.insert({ name: 'test3' });
+
+  Meteor._sleepForMs(100);
+
+  TestData.remove({ name: 'test2' });
+
+  Meteor._sleepForMs(100);
+
+  let metrics = Kadira.models.pubsub._getMetrics(new Date(), pub);
+
+  test.equal(metrics.totalObservers, 1);
+  test.equal(metrics.liveRemovedDocuments, 1);
+
+  TestData.remove({});
+
+  Meteor._sleepForMs(100);
+
+  metrics = Kadira.models.pubsub._getMetrics(new Date(), pub);
+
+  test.equal(metrics.totalObservers, 1);
+  test.equal(metrics.liveRemovedDocuments, 3);
 
   sub.stop();
   TestData.remove({});
