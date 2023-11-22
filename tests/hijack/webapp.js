a9ba3b2937adde8bd565090e364ff3bc2f815709
Leonardo Venturini <leovenbag@gmail.com>
2023-11-22 09:07:07 -0400

    wrap webapp middleware

--- tests/hijack/webapp.js
+++ tests/hijack/webapp.js
@@ -1,6 +1,22 @@
 import { WebApp } from 'meteor/webapp';
 import { addAsyncTest } from '../_helpers/helpers';
+import { TestData } from '../_helpers/globals';
+import { getInfo } from '../../lib/async/als';
 
+let trace = null;
+
+async function middleware (req, res, next) {
+  await TestData.find().fetch();
+
+  next();
+}
+
+WebApp.connectHandlers.use('/test-middleware', middleware, (req, res) => {
+  trace = getInfo().trace;
+
+  res.writeHead(200);
+  res.end(`Hello world from: ${Meteor.release}`);
+});
 
 addAsyncTest(
   'Webapp - return connect app from .use',
@@ -45,3 +61,22 @@ addAsyncTest(
         });
     });
   });
+
+addAsyncTest(
+  'Webapp - find in middleware',
+  async function (test) {
+    const result = await fetch(`${Meteor.absoluteUrl()}/test-middleware`);
+
+    const event = trace.events.find(([type]) => type === 'db');
+
+    test.equal(result.status, 200);
+    test.stableEqual(event[2], {
+      coll: 'tinytest-data',
+      selector: '{}',
+      func: 'fetch',
+      cursor: true,
+      docSize: 0,
+      docsFetched: 0
+    });
+  }
+);
