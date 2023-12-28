a3ae6baa2f29b5e1a197800ae6f8d08ac4534a4e
Renan <renanccastro@gmail.com>
2023-12-28 16:01:33 -0300

    fix: tests run without redis-oplog so we have to check it when initializing globals

--- tests/_helpers/globals.js
+++ tests/_helpers/globals.js
@@ -3,6 +3,6 @@ import { Meteor } from 'meteor/meteor';
 export const MethodStore = [];
 export const TestData = new Meteor.Collection('tinytest-data');
 export const TestDataRedis = new Meteor.Collection('tinytest-data-redis');
-TestDataRedis.configureRedisOplog({});
+TestDataRedis.configureRedisOplog?.({});
 export const TestDataRedisNoRaceProtection = new Meteor.Collection('tinytest-data-redis-NoRaceProtection');
-TestDataRedisNoRaceProtection.configureRedisOplog({protectAgainstRaceConditions: false});
+TestDataRedisNoRaceProtection.configureRedisOplog?.({protectAgainstRaceConditions: false});
