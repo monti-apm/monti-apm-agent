ecd17b27cd0601e2fc8c8ca8d9731bf00270cbc2
zodern <zodern@users.noreply.github.com>
2025-01-19 13:57:46 -0500

    v2.50.1-beta.1

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
   summary: 'Performance Monitoring for Meteor',
-  version: '2.50.0',
+  version: '2.50.1-beta.1',
   git: 'https://github.com/monti-apm/monti-apm-agent.git',
   name: 'montiapm:agent'
 });
