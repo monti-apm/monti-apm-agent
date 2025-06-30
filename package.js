2effdc8e0bd776be2dee38208dbd9d791ed95517
zodern <zodern@users.noreply.github.com>
2025-06-30 17:20:48 -0400

    Remove connect wrapping for old json-routes

--- package.js
+++ package.js
@@ -115,7 +115,6 @@ function configurePackage (api, isTesting) {
   }
 
   api.use('meteorhacks:zones@1.2.1', { weak: true });
-  api.use('simple:json-routes@2.1.0', { weak: true });
   api.use('zodern:meteor-package-versions@0.2.0');
   api.use('zodern:types@1.0.13');
 
